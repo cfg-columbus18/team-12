66719643eef71c3b950899c3627695bd3e121a56
Santiago Salazar <salazars1@xavier.edu>
2018-10-19 19:27:44 -0400

    Add files via upload

---
+++ Code4GoodRefugeeMentorMentee/Code4GoodRefugeeMentorMentee/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Code4GoodRefugeeMentorMentee.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
